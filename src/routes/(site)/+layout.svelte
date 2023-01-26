16fb5bf834eb3d8d043bfdc7749375963b6a8b8d
Andrew Chalupa <andrewchalupa@pm.me>
2023-01-26 03:37:36 -0500

    basic changes

---
+++ src/routes/(site)/+layout.svelte
@@ -0,0 +1,59 @@
+<div class="header">
+	<div class="nav-bar">
+		<div class="nav-section nav-logo">
+			<div><img src="/mf_logo.png" alt="mapfeed logo" /></div>
+			<div><a href="/">MapFeed</a></div>
+		</div>
+		<div class="nav-section nav-details">
+			<div class="nav-pricing"><a href="/pricing">Pricing</a></div>
+		</div>
+		<div class="nav-section nav-register">
+			<a href="/sign-in"><div class="nav-signin">Sign In</div></a>
+			<a href="/sign-up"><div class="nav-signin">Sign Up</div></a>
+		</div>
+	</div>
+</div>
+
+<slot />
+
+<style lang="sass">
+    a
+        text-decoration: none
+        color: inherit
+
+    .header 
+        position: absolute
+        width: 100%
+        top: 0
+        color: #fabc04
+
+        .nav-bar
+            display: flex
+            align-items: center
+            padding: 12px 32px
+            justify-content: space-between
+
+        .nav-section 
+            flex: 1
+            display: flex
+
+        .nav-details
+            display: flex
+            justify-content: center
+
+        .nav-register
+            display: flex
+            justify-content: right
+            gap: 12px
+
+        .nav-logo
+            font-size: 30px
+            font-weight: 700
+            display: flex
+            align-items: center
+            gap: 12px
+
+            img
+                width: 50px
+                height: 50px
+</style>
---
+++ src/routes/(site)/+page.svelte
@@ -0,0 +1,19 @@
+<div class="home">
+	<div class="catchphrase">Discover the World,<br /> all from one place</div>
+	<div class="map-img"><img src="./static/map-home.png" alt="world map" /></div>
+</div>
+
+<style lang="sass">
+	.home
+		height: 100%
+		background: #0e430d
+
+	.catchphrase
+		font-size: 60px
+		color: #fabc04
+		height: 100%
+		display: flex
+		align-items: center
+		
+
+</style>
