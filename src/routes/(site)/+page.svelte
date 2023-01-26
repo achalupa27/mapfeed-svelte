8741281313e2ed8b74b24b0c4a5d9a7b0e88f35c
Andrew Chalupa <andrewchalupa@pm.me>
2023-01-26 03:42:31 -0500

    basic changes

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -1,12 +1,15 @@
 <div class="home">
-	<div class="catchphrase">Discover the World,<br /> all from one place</div>
-	<div class="map-img"><img src="./static/map-home.png" alt="world map" /></div>
+	<div class="catchphrase">Discover the World,<br /> all from one place.</div>
+	<div class="map-img"><img src="/map-home.png" alt="world map" /></div>
 </div>
 
 <style lang="sass">
 	.home
 		height: 100%
 		background: #0e430d
+		display: flex
+		justify-content: space-around
+		align-items: center
 
 	.catchphrase
 		font-size: 60px
